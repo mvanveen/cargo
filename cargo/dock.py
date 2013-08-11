26649554f731a69256f4447bf68d1eaa765dcbae
Michael Van Veen <michael@mvanveen.net>
2013-08-11 10:08:07 +0000

    fix cargo precedence; code cleanup

--- cargo/dock.py
+++ cargo/dock.py
@@ -3,6 +3,9 @@ import docker
 LOCAL_URL = 'http://localhost:4243'
 DEFAULT_VERSION = "1.3"
 
+# this is a hack to get `__getattribute__` working for a few reserved properties
+RESERVED_METHODS = ['containers']
+
 class Dock(object):
   """Wrapper class for `docker-py` Client instances"""
   
@@ -18,9 +21,17 @@ class Dock(object):
   def __getattribute__(self, x):
     client = super(Dock, self).__getattribute__('_client')
 
-    # return client attribute if not a magic method
-    if hasattr(client, x) and not x.startswith('_'):
+
+    # return client attribute if not a magic method or reserved attr
+    legal = not x.startswith('_') and not(x in RESERVED_METHODS)
+
+    if hasattr(client, x) and legal:
       return client.__getattribute__(x)
 
     return super(Dock, self).__getattribute__(x)
+
+
+  @property
+  def containers(self, *args, **kw):
+    return self._client.containers(*args, **kw)
 
