71fd8a1dd140beed27a7ad3a66cd29c741b3a659
Michael Van Veen <michael@mvanveen.net>
2013-08-11 12:09:46 +0000

    core.py fix

--- cargo/core.py
+++ cargo/core.py
@@ -3,18 +3,11 @@ from cargo.dock import dock
 default_dock = Dock()
 
 def build(path=None, dock=default_dock, *args, **kw):
-  # catch an actual cargo type here
-  if len(args) and isinstance(args[0], Image):
-    # TODO: build image
-    image = args[0]
-    args = args[1:]
-    pass
-
-  return dock.build(*args, **kw)
+  raise NotImplementedError  
 
 
 def start(container=None, *args, **kw):
-  return docker.start
+  raise NotImplementedError  
 
 
 def ps(dock=default_dock):
