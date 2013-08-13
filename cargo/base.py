23f819297b37060d5a36eaafde0d3eec0fa47176
Michael Van Veen <michael@mvanveen.net>
2013-08-13 05:19:55 +0000

    extract Container constructor into base class

---
+++ cargo/base.py
@@ -0,0 +1,36 @@
+#from cargo.core import get_default_dock
+import cargo
+
+def lowercase(config):
+  return dict([(x.lower(), y) for x, y in config.iteritems()])
+
+class CargoBase(object):
+  """Base class for Cargo `Container` and `Image` classes"""
+
+  def __init__(self, *args, **kw):
+    super(CargoBase, self).__init__()
+
+    # if the first argument is the `docker-py` response dict, we want to 
+    # leverage that
+    if len(args) and isinstance(args[0], dict):
+      config = args[0]
+      config.update(kw)
+
+    elif len(args) and not isinstance(args[0], dict):
+      raise TypeError('expected container dictionary as first parameter') 
+    else:
+     config = kw
+
+    dock = kw.get('dock')
+    if dock and not isinstance(dock, cargo.Dock):
+      raise TypeError('expected dock parameter to be Dock object')
+
+    self._dock = dock or cargo.get_default_dock()
+
+    # response dict  from `docker-py` preserves caps.  we want 
+    # lowercase to correspond with keyword attrs
+    self._config = config = lowercase(config)
+
+  @property
+  def config():
+     raise NotImplementedError
--- cargo/container.py
+++ cargo/container.py
@@ -1,36 +1,15 @@
 import datetime
 
-import cargo
+from cargo.base import CargoBase
 
 def lowercase(config):
   return dict([(x.lower(), y) for x, y in config.iteritems()])
 
-class Container(object):
+class Container(CargoBase):
   """Python wrapper class encapsulating the metadata for a Docker Container"""
 
   def __init__(self, *args, **kw):
-    super(Container, self).__init__()
-
-    # if the first argument is the `docker-py` response dict, we want to 
-    # leverage that
-    if len(args) and isinstance(args[0], dict):
-      config = args[0]
-      config.update(kw)
-
-    elif len(args) and not isinstance(args[0], dict):
-      raise TypeError('expected container dictionary as first parameter') 
-    else:
-     config = kw
-
-    dock = kw.get('dock')
-    if dock and not isinstance(dock, cargo.Dock):
-      raise TypeError('expected dock parameter to be Dock object')
-
-    self._dock = dock or cargo.get_default_dock()
-
-    # response dict  from `docker-py` preserves caps.  we want 
-    # lowercase to correspond with keyword attrs
-    self._config = config = lowercase(config)
+    super(Container, self).__init__(*args, **kw)
  
   def __repr__(self):
     return '<Container [%s]>' % (self.container_id[:12],)
